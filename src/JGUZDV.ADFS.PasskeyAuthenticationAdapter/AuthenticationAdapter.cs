ce1637ff25f0f4284f0577a03ee47368f2305836
Ottenhus, Thomas <Ottenhus, Thomas>
2025-03-11 14:08:54 +0100

    Use out parameter

--- src/JGUZDV.ADFS.PasskeyAuthenticationAdapter/AuthenticationAdapter.cs
+++ src/JGUZDV.ADFS.PasskeyAuthenticationAdapter/AuthenticationAdapter.cs
@@ -12,248 +12,250 @@ using JGUZDV.ADFS.PasskeyAuthenticationAdapter.Extensions;
 
 using Microsoft.IdentityServer.Web.Authentication.External;
 
-namespace JGUZDV.ADFS.PasskeyAuthenticationAdapter
+namespace JGUZDV.ADFS.PasskeyAuthenticationAdapter;
+
+public class AuthenticationAdapter : IAuthenticationAdapter
 {
-    public class AuthenticationAdapter : IAuthenticationAdapter
+    private ConfigurationData _config = new();
+
+    public IAuthenticationAdapterMetadata Metadata => AuthenticationAdapterMetadata.Instance;
+
+
+    public void OnAuthenticationPipelineLoad(IAuthenticationMethodConfigData configData)
     {
-        private ConfigurationData _config = new();
-
-        public IAuthenticationAdapterMetadata Metadata => AuthenticationAdapterMetadata.Instance;
-
-
-        public void OnAuthenticationPipelineLoad(IAuthenticationMethodConfigData configData)
+        if (configData != null && configData.Data != null && configData.Data.Length > 0)
         {
-            if (configData != null && configData.Data != null && configData.Data.Length > 0)
+            try
             {
-                try
-                {
-                    var jsonContractSerializer = new DataContractJsonSerializer(typeof(ConfigurationData));
-                    _config = (ConfigurationData)jsonContractSerializer.ReadObject(configData.Data);
-                }
-                catch (Exception ex)
-                {
-                    throw new InvalidDataException("OnAuthenticationPipelineLoad [PasskeyAuthenticationAdapter]: Error parsing configuration data.", ex);
-                }
+                var jsonContractSerializer = new DataContractJsonSerializer(typeof(ConfigurationData));
+                _config = (ConfigurationData)jsonContractSerializer.ReadObject(configData.Data);
             }
-
-            if (_config.PasskeyHandlerUrl == null || !Uri.IsWellFormedUriString(_config.PasskeyHandlerUrl, UriKind.Absolute))
+            catch (Exception ex)
             {
-                throw new InvalidDataException("OnAuthenticationPipelineLoad [PasskeyAuthenticationAdapter]: BaseUrl was null or invalid");
-            }
-
-            if (string.IsNullOrWhiteSpace(_config.LdapServer))
-            {
-                throw new InvalidDataException("OnAuthenticationPipelineLoad [PasskeyAuthenticationAdapter]: LdapServer was null or empty");
-            }
-
-            if (string.IsNullOrWhiteSpace(_config.SearchBaseDN))
-            {
-                throw new InvalidDataException("OnAuthenticationPipelineLoad [PasskeyAuthenticationAdapter]: SearchBaseDN was null or empty");
+                throw new InvalidDataException("OnAuthenticationPipelineLoad [PasskeyAuthenticationAdapter]: Error parsing configuration data.", ex);
             }
         }
 
-        public void OnAuthenticationPipelineUnload() { }
-
-
-
-        public bool IsAvailableForUser(Claim identityClaim, IAuthenticationContext context)
+        if (_config.PasskeyHandlerUrl == null || !Uri.IsWellFormedUriString(_config.PasskeyHandlerUrl, UriKind.Absolute))
         {
-            if (string.IsNullOrWhiteSpace(identityClaim?.Value))
-            {
-                return false;
-            }
-
-            EnsurePasskeysInContext(identityClaim!.Value, context);
-            return context.HasPasskeyCredentialIds();
+            throw new InvalidDataException("OnAuthenticationPipelineLoad [PasskeyAuthenticationAdapter]: BaseUrl was null or invalid");
         }
 
-
-
-        public IAdapterPresentation? BeginAuthentication(Claim identityClaim, HttpListenerRequest request, IAuthenticationContext context)
+        if (string.IsNullOrWhiteSpace(_config.LdapServer))
         {
-            if (context == null)
-            {
-                throw new InvalidOperationException("BeginAuthentication [PasskeyAuthenticationAdapter]: context was null");
-            }
-
-            if (string.IsNullOrWhiteSpace(identityClaim?.Value))
-            {
-                throw new InvalidOperationException("BeginAuthentication [PasskeyAuthenticationAdapter]: identityClaim was null or empty");
-            }
-
-            context.Data["userPrincipalName"] = identityClaim!.Value;
-
-            EnsurePasskeysInContext(identityClaim!.Value, context);
-            if (!context.HasPasskeyCredentialIds())
-            {
-                throw new InvalidOperationException("BeginAuthentication [PasskeyAuthenticationAdapter]: No passkeys where associated with the user identity.");
-            }
-
-            var assertionOptions = GetPasskeyAssertionOptions(context);
-            context.SaveAssertionOptions(assertionOptions);
-
-            return new PasskeyPresentation(new()
-            {
-                ShouldAutostartFlow = true,
-                AssertionOptions = assertionOptions,
-            });
+            throw new InvalidDataException("OnAuthenticationPipelineLoad [PasskeyAuthenticationAdapter]: LdapServer was null or empty");
         }
 
-
-        public IAdapterPresentation? TryEndAuthentication(IAuthenticationContext context, IProofData proofData, HttpListenerRequest request, out Claim[] claims)
+        if (string.IsNullOrWhiteSpace(_config.SearchBaseDN))
         {
-            if (context == null)
-            {
-                throw new InvalidOperationException("TryEndAuthentication [PasskeyAuthenticationAdapter]: context was null");
-            }
-
-            if (proofData == null || proofData.Properties == null)
-            {
-                throw new InvalidOperationException("TryEndAuthentication [PasskeyAuthenticationAdapter]: no proof data received");
-            }
-
-            var passkeyClaims = new List<Claim>();
-            var passkeyValidationResult = ValidatePasskeyAssertion(context, proofData, passkeyClaims);
-            if (passkeyValidationResult)
-            {
-                // Add the authentication method claim mentioned in metadata, indicating the particulate authentication method has been used successfully.
-                passkeyClaims.Add(new Claim(ClaimTypes.AuthenticationMethod, Metadata.AuthenticationMethods.First()));
-                claims = passkeyClaims.ToArray();
-
-                return null;
-            }
-
-            string? userIdentity = context.Data["userPrincipalName"] as string 
-                ?? throw new InvalidOperationException("TryEndAuthentication [PasskeyAuthenticationAdapter]: userIdentity has been lost.");
-
-            EnsurePasskeysInContext(userIdentity, context);
-            if (!context.HasPasskeyCredentialIds())
-            {
-                throw new InvalidOperationException("BeginAuthentication [PasskeyAuthenticationAdapter]: No passkeys where associated with the user identity.");
-            }
-
-            var assertionOptions = GetPasskeyAssertionOptions(context);
-            context.SaveAssertionOptions(assertionOptions);
-
-            claims = Array.Empty<Claim>();
-            return new PasskeyPresentation(new()
-            {
-                ShouldAutostartFlow = false,
-                AssertionOptions = assertionOptions,
-                ErrorResource = "PasskeyAuthFailed"
-            });
+            throw new InvalidDataException("OnAuthenticationPipelineLoad [PasskeyAuthenticationAdapter]: SearchBaseDN was null or empty");
         }
+    }
+
+    public void OnAuthenticationPipelineUnload() { }
 
 
-        public IAdapterPresentation? OnError(HttpListenerRequest request, ExternalAuthenticationException ex)
+
+    public bool IsAvailableForUser(Claim identityClaim, IAuthenticationContext context)
+    {
+        if (string.IsNullOrWhiteSpace(identityClaim?.Value))
         {
-            return null; // This will appearantly trigger "default ADFS error handling"
-        }
-
-
-        private void EnsurePasskeysInContext(string userPrincipalName, IAuthenticationContext context)
-        {
-            if (context.HasPasskeyCredentialIds())
-            {
-                return;
-            }
-
-            var passkeyIds = ActiveDirectory.GetUserPasskeyIds(userPrincipalName, _config.SearchBaseDN!, _config.LdapServer!);
-            if (passkeyIds?.Any() == true)
-            {
-                context.SavePasskeyCredentialIds(passkeyIds);
-            }
-        }
-
-
-        private string GetPasskeyAssertionOptions(IAuthenticationContext context)
-        {
-            var credentialIds = context.GetPasskeyCredentialIds();
-
-            var uriBuilder = new UriBuilder(_config.PasskeyHandlerUrl);
-            uriBuilder.Query = string.Join("&", credentialIds.Select(x => $"pci={x}"));
-
-            var httpRequest = WebRequest.CreateHttp(uriBuilder.ToString());
-            httpRequest.Accept = "application/json";
-
-            using var response = (HttpWebResponse)httpRequest.GetResponse();
-            if (response.StatusCode != HttpStatusCode.OK)
-            {
-                throw new InvalidOperationException("GetPasskeyOptions [PasskeyAuthenticationAdapter]: Could not retrieve Passkey AssertionOptions from PasskeyHandler.");
-            }
-
-            using var stream = response.GetResponseStream();
-            using var reader = new StreamReader(stream, Encoding.UTF8);
-            {
-                var responseString = reader.ReadToEnd();
-                return responseString;
-            }
-        }
-
-        private bool ValidatePasskeyAssertion(IAuthenticationContext context, IProofData proofData, IList<Claim> passkeyClaims)
-        {
-            var assertionOptions = context.GetAssertionOptions();
-            var assertionResponse = proofData.Properties["assertionResponse"] as string;
-
-            if (string.IsNullOrWhiteSpace(assertionOptions) || string.IsNullOrWhiteSpace(assertionResponse))
-            {
-                return false;
-            }
-
-            StringBuilder postData = new StringBuilder();
-            AppendUrlEncoded(postData, "assertionOptions", assertionOptions!);
-            AppendUrlEncoded(postData, "assertionResponse", assertionResponse!);
-            var postBytes = Encoding.UTF8.GetBytes(postData.ToString());
-
-            var httpRequest = WebRequest.CreateHttp(_config.PasskeyHandlerUrl);
-            httpRequest.Method = "POST";
-            httpRequest.ContentType = "application/x-www-form-urlencoded";
-            httpRequest.ContentLength = postBytes.Length;
-
-            using (var stream = httpRequest.GetRequestStream())
-            {
-                stream.Write(postBytes, 0, postBytes.Length);
-                stream.Flush();
-            }
-
-            using var response = (HttpWebResponse)httpRequest.GetResponse();
-
-            if(response.StatusCode == HttpStatusCode.OK)
-            {
-                using var stream = response.GetResponseStream();
-                using var reader = new StreamReader(stream, Encoding.UTF8);
-                
-                var responseString = reader.ReadToEnd();
-                
-                var equalSign = new char[] { '=' };
-                var newLines = new char[] { '\r', '\n' };
-                foreach (var line in responseString.Split(newLines, StringSplitOptions.RemoveEmptyEntries))
-                {
-                    var elements = line.Trim().Split(equalSign, 2, StringSplitOptions.RemoveEmptyEntries);
-                    if(elements.Length != 2)
-                    {
-                        continue;
-                    }
-
-                    passkeyClaims.Add(new Claim(elements[0].Trim(), elements[1].Trim()));
-                }
-
-                return true;
-            }
-
             return false;
         }
 
-        private static void AppendUrlEncoded(StringBuilder sb, string name, string value)
+        EnsurePasskeysInContext(identityClaim!.Value, context);
+        return context.HasPasskeyCredentialIds();
+    }
+
+
+
+    public IAdapterPresentation? BeginAuthentication(Claim identityClaim, HttpListenerRequest request, IAuthenticationContext context)
+    {
+        if (context == null)
         {
-            if (sb.Length != 0)
+            throw new InvalidOperationException("BeginAuthentication [PasskeyAuthenticationAdapter]: context was null");
+        }
+
+        if (string.IsNullOrWhiteSpace(identityClaim?.Value))
+        {
+            throw new InvalidOperationException("BeginAuthentication [PasskeyAuthenticationAdapter]: identityClaim was null or empty");
+        }
+
+        context.Data["userPrincipalName"] = identityClaim!.Value;
+
+        EnsurePasskeysInContext(identityClaim!.Value, context);
+        if (!context.HasPasskeyCredentialIds())
+        {
+            throw new InvalidOperationException("BeginAuthentication [PasskeyAuthenticationAdapter]: No passkeys where associated with the user identity.");
+        }
+
+        var assertionOptions = GetPasskeyAssertionOptions(context);
+        context.SaveAssertionOptions(assertionOptions);
+
+        return new PasskeyPresentation(new()
+        {
+            ShouldAutostartFlow = true,
+            AssertionOptions = assertionOptions,
+        });
+    }
+
+
+    public IAdapterPresentation? TryEndAuthentication(IAuthenticationContext context, IProofData proofData, HttpListenerRequest request, out Claim[] claims)
+    {
+        if (context == null)
+        {
+            throw new InvalidOperationException("TryEndAuthentication [PasskeyAuthenticationAdapter]: context was null");
+        }
+
+        if (proofData == null || proofData.Properties == null)
+        {
+            throw new InvalidOperationException("TryEndAuthentication [PasskeyAuthenticationAdapter]: no proof data received");
+        }
+
+        var passkeyValidationResult = ValidatePasskeyAssertion(context, proofData, out var passkeyClaims);
+        if (passkeyValidationResult && passkeyClaims != null)
+        {
+            // Add the authentication method claim mentioned in metadata, indicating the particulate authentication method has been used successfully.
+            passkeyClaims.Insert(0, new Claim(ClaimTypes.AuthenticationMethod, Metadata.AuthenticationMethods.First()));
+            claims = passkeyClaims.ToArray();
+
+            return null;
+        }
+
+        string? userIdentity = context.Data["userPrincipalName"] as string 
+            ?? throw new InvalidOperationException("TryEndAuthentication [PasskeyAuthenticationAdapter]: userIdentity has been lost.");
+
+        EnsurePasskeysInContext(userIdentity, context);
+        if (!context.HasPasskeyCredentialIds())
+        {
+            throw new InvalidOperationException("BeginAuthentication [PasskeyAuthenticationAdapter]: No passkeys where associated with the user identity.");
+        }
+
+        var assertionOptions = GetPasskeyAssertionOptions(context);
+        context.SaveAssertionOptions(assertionOptions);
+
+        claims = Array.Empty<Claim>();
+        return new PasskeyPresentation(new()
+        {
+            ShouldAutostartFlow = false,
+            AssertionOptions = assertionOptions,
+            ErrorResource = "PasskeyAuthFailed"
+        });
+    }
+
+
+    public IAdapterPresentation? OnError(HttpListenerRequest request, ExternalAuthenticationException ex)
+    {
+        return null; // This will appearantly trigger "default ADFS error handling"
+    }
+
+
+    private void EnsurePasskeysInContext(string userPrincipalName, IAuthenticationContext context)
+    {
+        if (context.HasPasskeyCredentialIds())
+        {
+            return;
+        }
+
+        var passkeyIds = ActiveDirectory.GetUserPasskeyIds(userPrincipalName, _config.SearchBaseDN!, _config.LdapServer!);
+        if (passkeyIds?.Any() == true)
+        {
+            context.SavePasskeyCredentialIds(passkeyIds);
+        }
+    }
+
+
+    private string GetPasskeyAssertionOptions(IAuthenticationContext context)
+    {
+        var credentialIds = context.GetPasskeyCredentialIds();
+
+        var uriBuilder = new UriBuilder(_config.PasskeyHandlerUrl);
+        uriBuilder.Query = string.Join("&", credentialIds.Select(x => $"pci={x}"));
+
+        var httpRequest = WebRequest.CreateHttp(uriBuilder.ToString());
+        httpRequest.Accept = "application/json";
+
+        using var response = (HttpWebResponse)httpRequest.GetResponse();
+        if (response.StatusCode != HttpStatusCode.OK)
+        {
+            throw new InvalidOperationException("GetPasskeyOptions [PasskeyAuthenticationAdapter]: Could not retrieve Passkey AssertionOptions from PasskeyHandler.");
+        }
+
+        using var stream = response.GetResponseStream();
+        using var reader = new StreamReader(stream, Encoding.UTF8);
+        {
+            var responseString = reader.ReadToEnd();
+            return responseString;
+        }
+    }
+
+    private bool ValidatePasskeyAssertion(IAuthenticationContext context, IProofData proofData, out IList<Claim>? passkeyClaims)
+    {
+        var assertionOptions = context.GetAssertionOptions();
+        var assertionResponse = proofData.Properties["assertionResponse"] as string;
+
+        if (string.IsNullOrWhiteSpace(assertionOptions) || string.IsNullOrWhiteSpace(assertionResponse))
+        {
+            passkeyClaims = null;
+            return false;
+        }
+
+        StringBuilder postData = new StringBuilder();
+        AppendUrlEncoded(postData, "assertionOptions", assertionOptions!);
+        AppendUrlEncoded(postData, "assertionResponse", assertionResponse!);
+        var postBytes = Encoding.UTF8.GetBytes(postData.ToString());
+
+        var httpRequest = WebRequest.CreateHttp(_config.PasskeyHandlerUrl);
+        httpRequest.Method = "POST";
+        httpRequest.ContentType = "application/x-www-form-urlencoded";
+        httpRequest.ContentLength = postBytes.Length;
+
+        using (var stream = httpRequest.GetRequestStream())
+        {
+            stream.Write(postBytes, 0, postBytes.Length);
+            stream.Flush();
+        }
+
+        using var response = (HttpWebResponse)httpRequest.GetResponse();
+
+        if(response.StatusCode == HttpStatusCode.OK)
+        {
+            passkeyClaims = new List<Claim>();
+
+            using var stream = response.GetResponseStream();
+            using var reader = new StreamReader(stream, Encoding.UTF8);
+            
+            var responseString = reader.ReadToEnd();
+            
+            var equalSign = new char[] { '=' };
+            var newLines = new char[] { '\r', '\n' };
+            foreach (var line in responseString.Split(newLines, StringSplitOptions.RemoveEmptyEntries))
             {
-                sb.Append("&");
+                var elements = line.Trim().Split(equalSign, 2, StringSplitOptions.RemoveEmptyEntries);
+                if(elements.Length != 2)
+                {
+                    continue;
+                }
+
+                passkeyClaims.Add(new Claim(elements[0].Trim(), elements[1].Trim()));
             }
 
-            sb.Append(HttpUtility.UrlEncode(name));
-            sb.Append("=");
-            sb.Append(HttpUtility.UrlEncode(value));
+            return true;
         }
+
+        passkeyClaims = null;
+        return false;
+    }
+
+    private static void AppendUrlEncoded(StringBuilder sb, string name, string value)
+    {
+        if (sb.Length != 0)
+        {
+            sb.Append("&");
+        }
+
+        sb.Append(HttpUtility.UrlEncode(name));
+        sb.Append("=");
+        sb.Append(HttpUtility.UrlEncode(value));
     }
 }
